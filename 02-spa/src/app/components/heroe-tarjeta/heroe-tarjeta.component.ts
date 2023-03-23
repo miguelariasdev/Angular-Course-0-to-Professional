c881bdb35c7c0d6eb32b636efe76fa2a8560054b
MIGUEL ÁNGEL ARIAS SÁNCHEZ <miguelariasdev@gmail.com>
2023-03-23 15:26:35 +0100

    .

--- 02-spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ 02-spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -10,12 +11,16 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe:any = {};
   @Input() index:number = 0;
 
-  constructor(){
+  constructor(private router:Router) { }
 
-  }
 
   ngOnInit(){
 
+  }
+
+  verHeroe(){
+    console.log(this.index);
+    this.router.navigate(['/heroe', this.index]);
   }
 
 }
